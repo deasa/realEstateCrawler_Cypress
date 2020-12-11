b51c5692a6d60976739bf74c4a90b5ccd85f8968
Brendan Ashton <bgeorgeashton@gmail.com>
2020-12-11 16:00:40 -0700

    opening webpage and performing search

--- cypress/integration/examples/newPlats.js
+++ cypress/integration/examples/newPlats.js
@@ -2,6 +2,17 @@
 
 context('new plats', () => {
     beforeEach(() => {
-        cy.visit('https://example.cypress.io/commands/actions')
+        cy.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+      })
+        cy.visit('/DocKoiForm.asp')
     })
+
+    it('go to webpage', () => {
+        cy.get('input[name=avKoi]').should('be.visible').type('S PLAT')
+        cy.get('input[name=avEntryDate]').should('be.visible').type('11/01/2020{enter}')
+        cy.get('input[name=Submit]').click()
+    });
 });
